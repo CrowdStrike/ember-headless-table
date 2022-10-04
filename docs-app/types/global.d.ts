2c39acd16fc77d5e78da9c0df3596a781994f5a5
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-04 18:21:46 -0400

    chore(types): get type checking passing

--- docs-app/types/global.d.ts
+++ docs-app/types/global.d.ts
@@ -1,11 +1,3 @@
-// Types for compiled templates
-declare module 'docs-app/templates/*' {
-  import type { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
-
 declare module '*.css' {
   const styles: { [className: string]: string };
   export default styles;
@@ -16,6 +8,5 @@ declare module '@ember/helper';
 declare module '@ember/modifier';
 
 // Types for these do not exist
-declare module 'highlightjs-glimmer';
 declare module 'highlightjs-glimmer/vendor/highlight.js';
 declare module 'highlightjs-glimmer/vendor/javascript.min';
