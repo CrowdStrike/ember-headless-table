bbad54cb6a9b8a4ba226aafde91d1d747bc11f3c
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-06 00:04:34 -0400

    chore(tests): remove the webpack hack -- it'll be better to remove sinon entirely, see: #7

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -6,15 +6,6 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     autoImport: {
       watchDependencies: ['ember-headless-table'],
-      webpack: {
-        resolve: {
-          fallback: {
-            // Sinon is not browser compatible...
-            // We should stop using sinon...
-            util: require.resolve('util/')
-          }
-        }
-      }
     },
   });
 
