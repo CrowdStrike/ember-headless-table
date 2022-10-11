88ff7e1fc919ff8fd3e858f7a7e32c3bbf22bbc9
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-11 14:47:53 -0400

    chore(sticky-columns): fix types for sticky helpers

--- ember-headless-table/src/plugins/sticky-columns/helpers.ts
+++ ember-headless-table/src/plugins/sticky-columns/helpers.ts
@@ -3,7 +3,9 @@ import { StickyColumns } from './plugin';
 
 import type { Column } from '[public-types]';
 
-export const isSticky = (column: Column) => meta.forColumn(column, StickyColumns).isSticky;
+export const isSticky = <DataType = unknown>(column: Column<DataType>) =>
+  meta.forColumn(column, StickyColumns).isSticky;
 
-export const styleFor = (column: Column): Partial<CSSStyleDeclaration> =>
-  meta.forColumn(column, StickyColumns).style;
+export const styleFor = <DataType = unknown>(
+  column: Column<DataType>
+): Partial<CSSStyleDeclaration> => meta.forColumn(column, StickyColumns).style;
