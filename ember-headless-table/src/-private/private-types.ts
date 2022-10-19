e6d31cc365fbefe4968afc2fee05f95c665f37a5
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-19 11:36:00 -0400

    chore(types): remove commented out type

--- ember-headless-table/src/-private/private-types.ts
+++ ember-headless-table/src/-private/private-types.ts
@@ -3,5 +3,4 @@
  */
 
 export type Constructor<T, Args extends any[] = any[]> = new (...args: Args) => T;
-// export type Constructor<T, Args extends any[] = any[]> = abstract new (...args: Args) => T;
 export type Class<T> = Constructor<T>;
